ff74eb5d0357c4bd4b13902a85bbbb5bfdb63e48
David Anderson <dlande000@gmail.com>
2019-08-12 19:15:51 -0400

    Add check for file type

--- random-item-generator.py
+++ random-item-generator.py
@@ -4,16 +4,19 @@ import random
 print("Enter a file path:")
 file_path = raw_input()
 
-with open(file_path, 'r') as csvfile:
-    csv_reader = csv.reader(csvfile)
-    items = {}
-    counter = 1
+if (file_path[-3:] != "csv"):
+    print("Please select a .csv file.")
+else:
+    with open(file_path, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        items = {}
+        counter = 1
+        
+        for line in csv_reader:
+            if len(line) == 1:
+                items[counter] = line[0]
+            else:
+                items[counter] = line
+            counter += 1
 
-    for line in csv_reader:
-        if len(line) == 1:
-            items[counter] = line[0]
-        else:
-            items[counter] = line
-        counter += 1
-    
     print(items[random.randint(1,len(items))])

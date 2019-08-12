0df2b30def35cffd5083823ed08d6be67add1e00
David Anderson <dlande000@gmail.com>
2019-08-12 18:31:34 -0400

    Add hashmap

--- random-item-generator.py
+++ random-item-generator.py
@@ -2,6 +2,11 @@ import csv
 
 with open('spreadsheet.csv', 'r') as csvfile:
     csv_reader = csv.reader(csvfile)
-    
+    items = {}
+    counter = 1
+
     for line in csv_reader:
-        print(line)
+        items[counter] = line
+        counter += 1
+    
+    print(items)

467d4e24c2f7e2f923ced91402400e219cf34bce
David Anderson <dlande000@gmail.com>
2019-08-12 19:03:58 -0400

    Check for line length

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# random-item-generator
+# Random Item Generator
+
+Because I have enormous lists of movies to watch and books to read, and because I get overwhelmed choosing from these lists, I wrote this code which will generate a random item from a given list. 
+
+When run in the command line, the code will ask for a filepath to a CSV file. 
--- random-item-generator.py
+++ random-item-generator.py
@@ -1,19 +1,19 @@
 import csv
 import random
 
-def random_item_generator ():
-    print("Enter a file path:")
-    file_path = raw_input()
+print("Enter a file path:")
+file_path = raw_input()
 
-    with open(file_path, 'r') as csvfile:
-        csv_reader = csv.reader(csvfile)
-        items = {}
-        counter = 1
+with open(file_path, 'r') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    items = {}
+    counter = 1
 
-        for line in csv_reader:
+    for line in csv_reader:
+        if len(line) == 1:
             items[counter] = line[0]
-            counter += 1
-        
-        print(items[random.randint(1,len(items))])
-
-random_item_generator()
+        else:
+            items[counter] = line
+        counter += 1
+    
+    print(items[random.randint(1,len(items))])
